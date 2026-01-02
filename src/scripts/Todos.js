f0b8216b75f90e304ad6891aeb9feddeb30f17f2
sahiba <25sahiba03@gmail.com>
2026-01-02 17:11:55 +0530

    set style

--- src/scripts/Todos.js
+++ src/scripts/Todos.js
@@ -1,13 +1,13 @@
-import {reloadAlltask} from "./reload.js"
-import{runslider} from "./slider.js";
-import {createTodoTask,createProgressTask,createCompletedTask,createSignTasks,createBacklogTask} from "./createTasks.js";
+import { reloadAlltask } from "./reload.js"
+import { runslider } from "./slider.js";
+import { createTodoTask, createProgressTask, createCompletedTask, createSignTasks, createBacklogTask } from "./createTasks.js";
 
-import {attachTodoBacklog,attachProgressToTodo,attachCompletedToProgress,attachSignToCompleted} from "./moveBackTask.js";
-import {attachTodoProgress,attachProgressToCompleted,attachCompletedToSign,attachBacklogToTodo} from "./moveforwardTask.js";
-import {attachTodoDelete,attachProgressDelete,attachCompletedDelete,attachBacklogDelete,attachSignDelete} from "./deleteTask.js";
+import { attachTodoBacklog, attachProgressToTodo, attachCompletedToProgress, attachSignToCompleted } from "./moveBackTask.js";
+import { attachTodoProgress, attachProgressToCompleted, attachCompletedToSign, attachBacklogToTodo } from "./moveforwardTask.js";
+import { attachTodoDelete, attachProgressDelete, attachCompletedDelete, attachBacklogDelete, attachSignDelete } from "./deleteTask.js";
 
-import{addButtonTask } from "./addBtn.js";
-import{handleSearchListItem} from "./searchTask.js";
+import { addButtonTask } from "./addTask.js";
+import { handleSearchListItem } from "./searchTask.js";
 
 export let taskInput = document.querySelector("#taskInput");
 export let addBtn = document.querySelector("#addBtn");
@@ -18,11 +18,11 @@ export let progressList = document.querySelector("#progressList");
 export let completedList = document.querySelector("#completedList");
 export let delayList = document.querySelector("#delayList");
 export let signList = document.querySelector("#signList");
- 
+
 let searchBtn = document.querySelector("#searchBtn");
 
- export let todoListData = [];
- export let progressListData = [];
+export let todoListData = [];
+export let progressListData = [];
 export let completedListData = [];
 export let delayListData = [];
 export let signListData = [];
@@ -40,33 +40,32 @@ function resetUpdateIndexes() {
   updateBacklogTaskIndex = -1;
 }
 //create a new task when i click the add Task button
-window.addEventListener("DOMContentLoaded", ()=> {
-   reloadAlltask(); 
-   runslider();
+window.addEventListener("DOMContentLoaded", () => {
+  reloadAlltask();
+  runslider();
 });
-addBtn.addEventListener("click",  ()=> {
+addBtn.addEventListener("click", () => {
   var inputboxText = taskInput.value.trim();
   var dueDatevalue = dueDate.value;
-addButtonTask(inputboxText,taskInput,dueDate,dueDatevalue ,delayListData,updateTodoTaskIndex,progressListData,updateprogressTaskIndex,updateDoneTaskIndex ,updateBacklogTaskIndex,todoListData,completedListData,createTodoList,createProgressList,createCompletedList,createDelayList,resetUpdateIndexes,handleClearTask);
+  addButtonTask(inputboxText, taskInput, dueDate, dueDatevalue, delayListData, updateTodoTaskIndex, progressListData, updateprogressTaskIndex, updateDoneTaskIndex, updateBacklogTaskIndex, todoListData, completedListData, createTodoList, createProgressList, createCompletedList, createDelayList, resetUpdateIndexes, handleClearTask);
 });
 export function createTodoList() {
   todoList.innerHTML = "";
-  document.querySelector("#tTaskHeading").children[0].innerHTML =   todoListData.length;
-   for (let i = 0; i < todoListData.length; i++) 
-    {
-    todoList.insertAdjacentHTML( "beforeend", createTodoTask({ title: todoListData[i].title, createDate: todoListData[i].createDate, update: todoListData[i].update,  due: todoListData[i].due })    );
+  document.querySelector("#tTaskHeading").children[0].innerHTML = todoListData.length;
+  for (let i = 0; i < todoListData.length; i++) {
+    todoList.insertAdjacentHTML("beforeend", createTodoTask({ title: todoListData[i].title, createDate: todoListData[i].createDate, update: todoListData[i].update, due: todoListData[i].due }));
   }
   let todoListItem = document.querySelectorAll(".todoListItem");
   for (let i = 0; i < todoListData.length; i++) {
-    let index = i;  
-    attachTodoBacklog(todoListItem[index], index, todoListData, delayListData ,createDelayList,createTodoList);
-    attachTodoProgress(todoListItem[index], index, todoListData, progressListData,createTodoList,createProgressList);
-    attachTodoDelete(todoListItem[index], index, todoListData,createTodoList);
-      
-     todoListItem[i].querySelector(".todoEditButton").addEventListener("click", todoEditList);
+    let index = i;
+    attachTodoBacklog(todoListItem[index], index, todoListData, delayListData, createDelayList, createTodoList);
+    attachTodoProgress(todoListItem[index], index, todoListData, progressListData, createTodoList, createProgressList);
+    attachTodoDelete(todoListItem[index], index, todoListData, createTodoList,taskInput,dueDate);
+
+    todoListItem[i].querySelector(".todoEditButton").addEventListener("click", todoEditList);
     function todoEditList() {
-      taskInput.value =  todoListData[i].title;
-      dueDate.value = todoListData[i].due;     
+      taskInput.value = todoListData[i].title;
+      dueDate.value = todoListData[i].due;
       updateTodoTaskIndex = i;
     }
   }
@@ -76,53 +75,46 @@ export function createProgressList() {
   progressList.innerHTML = "";
   document.querySelector("#pTaskHeading").children[0].innerHTML = progressListData.length;
 
-  for (let i = 0; i < progressListData.length; i++)
- {
-    progressList.insertAdjacentHTML( "beforeend", createProgressTask({ title: progressListData[i].title, createDate: progressListData[i].createDate, update: progressListData[i].update, due: progressListData[i].due,}) );
+  for (let i = 0; i < progressListData.length; i++) {
+    progressList.insertAdjacentHTML(
+      "beforeend", createProgressTask({ title: progressListData[i].title, createDate: progressListData[i].createDate, update: progressListData[i].update, due: progressListData[i].due, })
+    );
   }
   let progressListItem = document.querySelectorAll(".progressListItem");
-
   for (let i = 0; i < progressListData.length; i++) {
     let index = i;
-    attachProgressDelete( progressListItem[index], index, progressListData, createProgressList);
-    attachProgressToTodo(progressListItem[index],index,progressListData, todoListData, createProgressList, createTodoList);
-    attachProgressToCompleted( progressListItem[index], index, progressListData,completedListData, createProgressList, createCompletedList);
+    attachProgressDelete(progressListItem[index], index, progressListData, createProgressList,taskInput,dueDate);
+    attachProgressToTodo(progressListItem[index], index, progressListData, todoListData, createProgressList, createTodoList);
+    attachProgressToCompleted(progressListItem[index], index, progressListData, completedListData, createProgressList, createCompletedList);
 
     progressListItem[index].querySelector(".progressEditButton").addEventListener("click", function () {
-        taskInput.value = progressListData[index].title;
-        dueDate.value = progressListData[index].due;
-        updateprogressTaskIndex = index;
-      });
+      taskInput.value = progressListData[index].title;
+      dueDate.value = progressListData[index].due;
+      updateprogressTaskIndex = index;
+    });
   }
 }
 export function createCompletedList() {
   completedList.innerHTML = "";
-  document.querySelector("#dTaskHeading").children[0].innerHTML =
-    completedListData.length;
-
+  document.querySelector("#dTaskHeading").children[0].innerHTML =  completedListData.length;
+  
   for (let i = 0; i < completedListData.length; i++) {
     completedList.insertAdjacentHTML(
-      "beforeend",
-      createCompletedTask({
-        title: completedListData[i].title,
-        createDate: completedListData[i].createDate,
-        update: completedListData[i].update,
-        due: completedListData[i].due,
-      })
+      "beforeend",createCompletedTask({ title: completedListData[i].title, createDate: completedListData[i].createDate, update: completedListData[i].update, due: completedListData[i].due,})
     );
   }
   let completedListItem = document.querySelectorAll(".completedListItem");
   for (let i = 0; i < completedListData.length; i++) {
     let index = i;
-    attachCompletedDelete(completedListItem[index], index, completedListData, createCompletedList);
-    attachCompletedToProgress(completedListItem[index],index, completedListData, progressListData, createCompletedList, createProgressList);
+    attachCompletedDelete(completedListItem[index], index, completedListData, createCompletedList,taskInput,dueDate);
+    attachCompletedToProgress(completedListItem[index], index, completedListData, progressListData, createCompletedList, createProgressList);
     attachCompletedToSign(completedListItem[index], index, completedListData, signListData, createCompletedList, createSignList);
 
     completedListItem[index].querySelector(".completedEditButton").addEventListener("click", function () {
-        taskInput.value = completedListData[index].title;
-        dueDate.value = completedListData[index].due;
-        updateDoneTaskIndex = index;
-      });
+      taskInput.value = completedListData[index].title;
+      dueDate.value = completedListData[index].due;
+      updateDoneTaskIndex = index;
+    });
   }
 }
 export function createSignList() {
@@ -131,14 +123,14 @@ export function createSignList() {
     signListData.length;
 
   for (let i = 0; i < signListData.length; i++) {
-    signList.insertAdjacentHTML( "beforeend",createSignTasks({title: signListData[i].title, createDate: signListData[i].createDate, update: signListData[i].update, due: signListData[i].due, })
+    signList.insertAdjacentHTML("beforeend", createSignTasks({ title: signListData[i].title, createDate: signListData[i].createDate, update: signListData[i].update, due: signListData[i].due, })
     );
   }
   let signListItem = document.querySelectorAll(".signListItem");
   for (let i = 0; i < signListData.length; i++) {
     let index = i;
-    attachSignDelete(signListItem[index], index, signListData,createSignList);
-    attachSignToCompleted(signListItem[index],index,signListData,completedListData,createSignList,createCompletedList);
+    attachSignDelete(signListItem[index], index, signListData, createSignList,taskInput,dueDate);
+    attachSignToCompleted(signListItem[index], index, signListData, completedListData, createSignList, createCompletedList);
   }
 }
 export function createDelayList() {
@@ -146,31 +138,29 @@ export function createDelayList() {
   document.querySelector("#bTaskHeading").children[0].innerHTML =
     delayListData.length;
 
-  for (let i = 0; i < delayListData.length; i++)
-     {
-    delayList.insertAdjacentHTML( "beforeend", createBacklogTask({title: delayListData[i].title,createDate: delayListData[i].createDate,update: delayListData[i].update, due: delayListData[i].due, }) 
-  );
+  for (let i = 0; i < delayListData.length; i++) {
+    delayList.insertAdjacentHTML("beforeend", createBacklogTask({ title: delayListData[i].title, createDate: delayListData[i].createDate, update: delayListData[i].update, due: delayListData[i].due, })
+    );
   }
   let delayListItem = document.querySelectorAll(".delayListItem");
   for (let i = 0; i < delayListData.length; i++) {
     let index = i;
-    attachBacklogDelete(delayListItem[index], index, delayListData, createDelayList );
-    attachBacklogToTodo(delayListItem[index],index,delayListData,todoListData, createDelayList, createTodoList );
+    attachBacklogDelete(delayListItem[index], index, delayListData, createDelayList,taskInput,dueDate);
+    attachBacklogToTodo(delayListItem[index], index, delayListData, todoListData, createDelayList, createTodoList);
 
-    delayListItem[index] .querySelector(".backlogEditButton").addEventListener("click", function () {
-        taskInput.value = delayListData[index].title;
-        dueDate.value = delayListData[index].due;
-        updateBacklogTaskIndex = index;
-      });
+    delayListItem[index].querySelector(".backlogEditButton").addEventListener("click", function () {
+      taskInput.value = delayListData[index].title;
+      dueDate.value = delayListData[index].due;
+      updateBacklogTaskIndex = index;
+    });
   }
 }
-searchBtn.addEventListener("click", ()=>{
-var searchInput = document.querySelector("#searchInput");
-handleSearchListItem(searchInput,todoList, progressList, completedList, delayList, signList, todoListData, progressListData, completedListData, delayListData, signListData);
+searchBtn.addEventListener("click", () => {
+  var searchInput = document.querySelector("#searchInput");
+  handleSearchListItem(searchInput, todoList, progressList, completedList, delayList, signList, todoListData, progressListData, completedListData, delayListData, signListData);
 });
 clearBtn.addEventListener("click", handleClearTask);
- function handleClearTask() 
- {
+function handleClearTask() {
   searchInput.value = "";
   createTodoList();
   createProgressList();
--- src/scripts/deleteTask.js
+++ src/scripts/deleteTask.js
@@ -1,66 +1,50 @@
-export function attachTodoDelete(todoListItem, index, todoListData,createTodoList) {
-  todoListItem
-    .querySelector(".todoDeleteButton")
+export function attachTodoDelete(todoListItem, index, todoListData,createTodoList,taskInput,dueDate) {
+  todoListItem .querySelector(".todoDeleteButton")
     .addEventListener("click", function () {
       todoListData.splice(index, 1);
       localStorage.setItem("TODO", JSON.stringify(todoListData));
       createTodoList();
+      taskInput.value = "";
+      dueDate.value ="";
     });
 }
-export function attachProgressDelete(
-  progressListItem,
-  index,
-  progressListData,
-  createProgressList
-) {
-  progressListItem
-    .querySelector(".progressDeleteButton")
-    .addEventListener("click", function () {
+export function attachProgressDelete( progressListItem, index, progressListData, createProgressList,taskInput,dueDate) {
+  progressListItem.querySelector(".progressDeleteButton") .addEventListener("click", function () {
       progressListData.splice(index, 1);
       localStorage.setItem("PROGRESS", JSON.stringify(progressListData));
       createProgressList();
+       taskInput.value = "";
+      dueDate.value ="";
     });
 }
-export function attachCompletedDelete(
-  completedListItem,
-  index,
-  completedListData,
-  createCompletedList
-) {
-  completedListItem
-    .querySelector(".completedDeleteButton")
+export function attachCompletedDelete( completedListItem, index, completedListData, createCompletedList,taskInput,dueDate) {
+  completedListItem.querySelector(".completedDeleteButton")
     .addEventListener("click", function () {
       completedListData.splice(index, 1);
       localStorage.setItem("COMPLETED", JSON.stringify(completedListData));
       createCompletedList();
+       taskInput.value = "";
+      dueDate.value ="";
     });
 }
 
-export function attachBacklogDelete(
-  delayListItem,
-  index,
-  delayListData,
-  createDelayList
-) {
-  delayListItem
-    .querySelector(".delayDeleteButton")
+export function attachBacklogDelete(delayListItem, index, delayListData, createDelayList,taskInput,dueDate) {
+  delayListItem.querySelector(".delayDeleteButton")
     .addEventListener("click", function () {
       delayListData.splice(index, 1);
       localStorage.setItem("BACKLOG", JSON.stringify(delayListData));
       createDelayList();
+       taskInput.value = "";
+      dueDate.value ="";
     });
 }
-export function attachSignDelete(
-  signListItem,
-  index,
-  signListData,
-  createSignList
-) {
-  signListItem
-    .querySelector(".signDeleteButton")
+export function attachSignDelete( signListItem, index, signListData, createSignList,taskInput,dueDate) {
+  signListItem.querySelector(".signDeleteButton")
     .addEventListener("click", function () {
       signListData.splice(index, 1);
       localStorage.setItem("SIGN", JSON.stringify(signListData));
       createSignList();
+       taskInput.value = "";
+      dueDate.value ="";
     });
 }
