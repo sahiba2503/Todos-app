6b2223bdf5c64d0b3905681d6dd065008d7ee670
sahiba <25sahiba03@gmail.com>
2026-01-10 19:56:03 +0530

    get data from the server

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -4,44 +4,22 @@ var Btn = document.querySelector("#addBtn");
 var inputField = document.querySelector("#taskInput");
 var list = document.querySelector("ol");
  
-
-Btn.addEventListener("click",addTask);
-function addTask(){
-  var taskData = inputField.value;
-  var newtask = {
-    title:taskData
-   }
-   if(taskData == ""){
-    return;
-   }
-   
-  
-   else{
-     fetch(url,{
-    method:"POST",
-    body: JSON.stringify(newtask),
-   headers:{
-      "Content-Type":"application/json"
-   }
+   fetch(url,{
+    method:"GET"   
    })
-   .then(data=>data.json())
+   .then(res=>res.json())
    .then((data)=>{
-    console.log(data)
-    inputField.value = "";
-    createTaskItem(data);
-      })
-   .catch("error");
-   }
-
-};
-function createTaskItem(data){
-  list.insertAdjacentHTML("beforeend",`
+    for(let item of data){
+      if(item.id < 20){
+      console.log("task title is : " + item.title)
+        list.insertAdjacentHTML("beforeend",`
     <div class="taskDetail">
-    <li>${data.title} <li>
+    <li>${item.title} <li>
       <span class="upd">update</span>
       <span class="del">delete</span>
-</div>
-    `)
+</div>  `)
+    }
+  }
    
-}
-
+  })
+   .catch("error");
